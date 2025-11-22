a8e8a07059b4b203ba5e68a4e029216445ac6c74
Shaurya0987 <shaurya3537@gmail.com>
2025-11-22 22:12:46 +0530

    Details of apps stored

--- lib/screens/details/components/colorAndSize.dart
+++ lib/screens/details/components/colorAndSize.dart
@@ -1,51 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:preetykart/constants.dart';
 import 'package:preetykart/modals/product.dart';
-import 'package:preetykart/screens/details/components/project_title_with_image.dart';
-
-class BodyScreen extends StatelessWidget {
-  final Product product;
-  const BodyScreen({Key? key, required this.product}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            child: Stack(
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: size.height * 0.3),
-                  padding: EdgeInsets.only(
-                    top: size.height * 0.12,
-                    left: kDefultPaddin,
-                    right: kDefultPaddin,
-                  ),
-                  height: 500,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(24),
-                      topRight: Radius.circular(24),
-                    ),
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    children: [
-                      colorAndWidget(product: product),
-                    ],
-                  ),
-                ),
-                ProductTitleWithImage(product: product),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 class colorAndWidget extends StatelessWidget {
   const colorAndWidget({
